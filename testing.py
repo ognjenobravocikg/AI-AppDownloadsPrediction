66adef6ac5fe53038d04b243087fd3cf607a87be
Ognjen Obradović <94371789+ognjenobravocikg@users.noreply.github.com>
2024-08-24 14:55:00 +0200

    Add files via upload

--- testing.py
+++ testing.py
@@ -11,13 +11,10 @@ X = data.drop('Maximum Installs', axis=1)
 # Take 100 examples for testing
 _, X_test, _, y_test = train_test_split(X, y, test_size=1000, random_state=42)
 
-# Load the trained model
 model = joblib.load('final-results/main_model.joblib')
 
-# Make predictions on the test set
 y_pred = model.predict(X_test)
 
-# Display the first 30 predictions and their actual values
 comparison = pd.DataFrame({'Predicted': y_pred, 'Actual': y_test})
 print(comparison.head(100))
 
